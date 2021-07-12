2518a76f0ea099d27042a2f924fb2a58076108eb
kiranpalsingh1806 <kiranpalsingh1806@gmail.com>
2021-07-12 10:35:46 +0530

    Styled Recipe Container

--- src/App.js
+++ src/App.js
@@ -18,7 +18,37 @@ const RecipeContainer = styled.div`
     display:flex;
     flex-direction: column;
     padding: 10px;
+    width: 300px;
     box-shadow: 0 3px 10px 0 #aaa;
+`
+
+const CoverImage = styled.img`
+    height:200px;
+`
+
+const RecipeName = styled.span`
+    font-size:18px;
+    font-weight: bold;
+    color: black;
+    margin: 10px 0;
+`
+
+const IngredientsText = styled.span`
+    font-size:18px;
+    border: solid 1px green;
+    font-weight: bold;
+    color: black;
+    margin-bottom: 12px;
+    cursor: pointer;
+    padding: 10px 15px;
+    border-radius: 4px;
+    color: green;
+    text-align: center;
+`
+
+const SeeMoreText = styled(IngredientsText)`
+    color:#eb3300;
+    border: solid 1px #eb3300
 `
 
 function App() {
@@ -38,10 +68,10 @@ function App() {
 
       <RecipeListContainer>
           <RecipeContainer>
-              <img/>
-              <span> Matar Paneer</span>
-              <span>Ingtedients</span>
-              <span>See Complete Recipe</span>
+              <img src="/AppIcon.svg"/>
+              <RecipeName> Kulwant's Paneer</RecipeName>
+              <IngredientsText>Amanpal's Ingredients</IngredientsText>
+              <SeeMoreText>See Complete Recipe</SeeMoreText>
           </RecipeContainer>
       </RecipeListContainer>
     </Container>
