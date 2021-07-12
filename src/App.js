88912f441b86ffdc1c4d8160c73456ab24a2657f
kiranpalsingh1806 <kiranpalsingh1806@gmail.com>
2021-07-12 09:14:23 +0530

    Added Search Input field and styled app icon

--- src/App.js
+++ src/App.js
@@ -9,18 +9,33 @@ const Header = styled.div`
   color: white;
   background-color: black;
   display:flex;
+  /* justify-content:space-between; */
   flex-direction: row;
   padding: 20px;
   font-size: 25px;
   font-weight: bold;
   box-shadow: 0 3px 6px 0 #555;
+  align-items: center;
 `;
 
 const AppNameComponent = styled.div`
-
+    display:flex;
+    align-items: center;
 `;
+
+const AppIcon = styled.img`
+  width:36px;
+  height:36px;
+  margin: 15px;
+`
 const SearchComponent = styled.div`
-   padding: 20px;
+   display: flex;
+   flex-direction: flow;
+   background-color: white;
+   padding: 15px;
+   margin-top:10px;
+   border-radius: 6px;
+   width: 50%;
 `;
 
 function App() {
@@ -28,11 +43,13 @@ function App() {
     <Container>
       <Header>
         <AppNameComponent>
+            <AppIcon src="/AppIcon.svg" />
             Recipe Finder
         </AppNameComponent>
       </Header>
       <SearchComponent>
-        <img src="/search-icon.svg"/>
+        <img src="/search-icon.svg "/>
+        <input />
       </SearchComponent>
     </Container>
   );
