83ba75d4ae0cff05e762f9c409bfc590377059cb
kiranpalsingh1806 <kiranpalsingh1806@gmail.com>
2021-07-12 08:58:20 +0530

    Added Heading and Search Icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction:column;
+`;
+
+const Header = styled.div`
+  color: white;
+  background-color: black;
+  display:flex;
+  flex-direction: row;
+  padding: 20px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+
+const AppNameComponent = styled.div`
+
+`;
+const SearchComponent = styled.div`
+   padding: 20px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      Recipe Finder
-    </div>
+    <Container>
+      <Header>
+        <AppNameComponent>
+            Recipe Finder
+        </AppNameComponent>
+      </Header>
+      <SearchComponent>
+        <img src="/search-icon.svg"/>
+      </SearchComponent>
+    </Container>
   );
 }
 
